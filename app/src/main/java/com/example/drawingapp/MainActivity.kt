7d38fe0967264b7fcb65bb70e2f8a18e5efd3a81
Zaqueu Santos <zaqueulima1@gmail.com>
2021-03-30 15:45:48 -0300

    feature downlaod draw

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -6,7 +6,10 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -20,6 +23,9 @@ import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     private var currentColor: Int = Color.BLACK
@@ -37,7 +43,8 @@ class MainActivity : AppCompatActivity() {
 
         ib_gallery.setOnClickListener {
             if (isReadStorageAllowed()) {
-                val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val pickPhotoIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
 
                 startActivityForResult(pickPhotoIntent, GALLERY_CODE)
             } else {
@@ -56,6 +63,15 @@ class MainActivity : AppCompatActivity() {
         ib_remake.setOnClickListener {
             drawing_view.remake()
         }
+
+        ib_save.setOnClickListener {
+            if (isReadStorageAllowed()) {
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
+            } else {
+                requestPermission()
+            }
+
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -66,9 +82,14 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Permission granted now you can read the storage",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
-                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
@@ -108,30 +129,33 @@ class MainActivity : AppCompatActivity() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.brush_size)
         val window = brushDialog.window
-        window?.setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        window?.setLayout(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
 
         val seek = brushDialog.sk_brush_size
 
         seek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-                override fun onProgressChanged(
-                    seekBar: SeekBar?,
-                    progress: Int,
-                    fromUser: Boolean
-                ) {
-                    mSizeBrush = progress
-                    seek.progress = mSizeBrush
-                }
+            override fun onProgressChanged(
+                seekBar: SeekBar?,
+                progress: Int,
+                fromUser: Boolean
+            ) {
+                mSizeBrush = progress
+                seek.progress = mSizeBrush
+            }
 
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    seek.progress = mSizeBrush
-                }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                seek.progress = mSizeBrush
+            }
 
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                    drawing_view.setSizeForBrush(mSizeBrush.toFloat())
-                    seek.progress = mSizeBrush
-                    brushDialog.dismiss()
-                }
-            })
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                drawing_view.setSizeForBrush(mSizeBrush.toFloat())
+                seek.progress = mSizeBrush
+                brushDialog.dismiss()
+            }
+        })
 
         brushDialog.show()
     }
@@ -156,10 +180,75 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
-    private fun isReadStorageAllowed() : Boolean {
-        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+    private fun isReadStorageAllowed(): Boolean {
+        val result =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
 
         return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap) : AsyncTask<Any, Void, String>() {
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+
+            if (mBitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val file =
+                        File(
+                            externalCacheDir!!.absoluteFile.toString()
+                                    + File.separator + "DrawApp_"
+                                    + System.currentTimeMillis() / 1000
+                                    + ".png"
+                        )
+
+                    val fos = FileOutputStream(file)
+                    fos.write(bytes.toByteArray())
+                    fos.close()
+                    result = file.absolutePath
+                } catch (err: java.lang.Exception) {
+                    result = ""
+                    err.printStackTrace()
+                }
+            }
+
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if (result?.isNotEmpty()!!) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "File saved successfully : $result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong while saving the file.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     companion object {
